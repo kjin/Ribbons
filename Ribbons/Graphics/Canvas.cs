cd62ee49de2e973d7bdd7980e1724e27c9fd9993
Kelvin Jin <kkj9@cornell.edu>
2014-07-13 16:45:35 -0700

    add some farty classes

---
+++ Ribbons/Graphics/Canvas.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Ribbons.Graphics
+{
+    public class Canvas
+    {
+        SpriteBatch 
+    }
+
+    public class Camera
+    {
+        public Vector3 Position { get; set; }
+        public Vector3 Rotation { get; set; }
+        public Vector3 Scale { get; set; }
+
+        public Matrix World { get; private set; }
+
+        private void UpdateWorld()
+        {
+            World = Matrix.CreateScale(Scale);
+        }
+    }
+}
