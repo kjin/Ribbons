8b2f6e858ccbda2b790f5b6f8625207521a95fb0
Danny Carpenter <djc423@cornell.edu>
2014-07-13 17:23:49 -0700

    added ground

--- Ribbons/Engine/Ground/Ground.cs
+++ Ribbons/Engine/Ground/Ground.cs
@@ -3,15 +3,41 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-public enum GroundType
-{
+using FarseerPhysics.Common;
+using FarseerPhysics.Dynamics;
+using FarseerPhysics.Factories;
 
-}
+using Ribbons.Utils;
+using Ribbons.Engine;
     
 
-namespace Ribbons.Engine
+namespace Ribbons.Engine.Ground
 {
-    class Ground
+
+    public enum GroundType { Ground, Miasma };
+
+    public struct GroundUserData
     {
+        public GroundType groundType;
+    }
+
+    public class Ground
+    {
+        public Ground(World world, PolygonF shape, GroundType type)
+        {
+            GroundUserData userData;
+            userData.groundType = type;
+
+            Vertices vertices = new Vertices(shape.points);
+
+            Body body = BodyFactory.CreateBody(world, userData);
+            FixtureFactory.AttachPolygon(vertices, GroundConstants.GROUND_DENSITY, body, userData);
+            body.IsStatic = true;
+
+            if (type == GroundType.Miasma)
+            {
+                body.IsSensor = true;
+            }
+        }
     }
 }
---
+++ Ribbons/Engine/PhysicsConstants.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ribbons.Engine
+{
+    public class GroundConstants
+    {
+        public static float GROUND_DENSITY = 1.0f;
+    }
+}
